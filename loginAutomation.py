240eba82030f39d2d8ec42faa6ae21b48e4413d9
NipunShaji <nipunshaji9447@gmail.com>
2019-12-12 11:44:56 +0530

    almost ready

--- loginAutomation.py
+++ loginAutomation.py
@@ -1,8 +1,10 @@
 
-keka_url = 'http://sayone.keka.com'
-net_url = 'https://172.16.16.16:8090/httpclient.html'
+keka_url = 'https://sayone.keka.com/'
+keka_post_login_url = 'https://sayone.keka.com/#/home'
+net_url = 'https://172.16.16.16:8090'
 
 import time
+import sys
 
 from selenium import webdriver as wd
 from selenium.webdriver.common.keys import Keys
@@ -13,108 +15,95 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import NoSuchElementException
 
 
-class is_logged_in_id(object):
-    def __init__(self,locator):
-        self.locator = locator
-
-    def __call__(self,driver):
+def Wait_by_id(id):
+    for i in range(320):
+        time.sleep(.5)
         try:
-            elem = driver.find_element_by_id(*self.locator)
-            return elem
-        except:
-            return False
-# # Read credentials from file
-# try:
-#     file = open("credentials.txt",'r')
-# except:
-#     print('Some problem with file reading')
-#     error = 1
-#
-# file.readline()
-# line = file.readline()
-# luname = line.strip().split(' ')[-1]
-# line = file.readline()
-# lpass = line.strip().split(' ')[-1]
-# file.readline()
-# line = file.readline()
-# kuname = line.strip().split(' ')[-1]
-# line = file.readline()
-# kpass = line.strip().split(' ')[-1]
-#
-# # Open a new firefox window
-browser = wd.Firefox()
-#
-# # Open web login url
-# browser.get(net_url)
-#
-# #check if Local is already logged in
-#
-#
-# user = browser.find_element_by_name('username')
-# passwd = browser.find_element_by_name('password')
-# user.send_keys(luname)
-# passwd.send_keys(lpass)
-# passwd.send_keys(Keys.ENTER)
-#
-# i = 0
-# while i < 15:
-#     #check if login is okay
-#     time.sleep(0.2)
-#     i+=1
-# else:
-#     print('Local Login Failed')
-#     browser.quit()
-#
-# ac(browser).key_down(Keys.CONTROL).send_keys('t').key_up(Keys.CONTROL).perform()
-# browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-browser.get(keka_url)
+            browser.find_element_by_id(id)
+            return True
+        except NoSuchElementException:
+            pass
+    else:
+        return False
 
-#check if keka is already loged in
+# Read credentials from file
 try:
-    elem = browser.find_element_by_id('userProfile')
-    print(elem)
-except NoSuchElementException:
-    print('No loged in!\nTrying to login to keka')
-    browser.find_element_by_id('email').send_keys('nipun@sayonetech.com')
-    browser.find_element_by_id('password').send_keys('Napster@123')
-    ac(browser).send_keys(Keys.ENTER).perform()
+    file = open("credentials.txt",'r')
+except:
+    print('Some problem with file reading')
+    error = 1
 
-time.sleep(10)
-elem = browser.find_element_by_id('logoutForm')
-print(elem)
+file.readline()
+line = file.readline()
+luname = line.strip().split(' ')[-1]
+line = file.readline()
+lpass = line.strip().split(' ')[-1]
+file.readline()
+line = file.readline()
+kuname = line.strip().split(' ')[-1]
+line = file.readline()
+kpass = line.strip().split(' ')[-1]
 
-# when logged in for first time check for logoutform
-# If user is already logged in, must check for logoutform and userProfile
-# Need to find a way to check if two elements is present, at the same time.
+file.close()
 
-# element = wc(browser, 20).until(
-#         is_logged_in_id("userProfile")
-#     )
-# print(element)
+# Open a new firefox window
+browser = wd.Firefox()
+# main_window = browser.current_window_handle
 
-# try:
-#     element = wc(browser, 10).until(
-#         ec.is_logged_in_id((By.ID, "userProfile"))
-#     )
-# except:
-#     print('Keka Login Failed')
-# finally:
-#     browser.quit()
+# Open web login url
+browser.get(net_url)
+while True:
+    if not Wait_by_id('loginbutton'):
+        print('Local login page didn\'t load properly.\nExitting..')
+        browser.quit()
+        sys.exit()
+    else:
+        break
 
+#check if Local is already logged in
+button = browser.find_element_by_id('loginbutton')
 
-#
-# browser.find_element_by_id('email').send_keys(kuname)
-# browser.find_element_by_id('password').send_keys(kpass)
-# ac(browser).send_keys(Keys.ENTER).perform()
-#
-# i = 0
-#
-# while i < 15:
-#     #check if login is okay
-#     time.sleep(0.2)
-#     i+=1
-# else:
-#     print('Keka Login Failed')
-#     browser.quit()
+if button.text == 'Login':
+    user = browser.find_element_by_name('username')
+    passwd = browser.find_element_by_name('password')
+    user.send_keys(luname)
+    passwd.send_keys(lpass)
+    passwd.send_keys(Keys.ENTER)
+    i = 0
+    while i < 320:
+        button = browser.find_element_by_id('loginbutton')
+        if button.text == 'Logout':
+            break
+        i+=1
+        time.sleep(.2)
+    else:
+        print('Local login failed.\nExiting..\n')
+        browser.quit()
+        sys.exit()
 
-# goto attendence page and activate webclockin
+time.sleep(2)
+browser.execute_script("window.open('{}')".format(keka_url))
+
+# browser.get(keka_url)
+while browser.current_url != keka_url:
+    if browser.current_url == keka_post_login_url:
+        print('Already Logged.\n')
+        break
+    else:
+        print('Not logged in!\nTrying to login to keka')
+        if Wait_by_id('email'):
+            browser.find_element_by_id('email').send_keys(kuname)
+            browser.find_element_by_id('password').send_keys(kpass)
+            ac(browser).send_keys(Keys.ENTER).perform()
+        else:
+            print('Page didn\'t load properly.\nExitting..')
+            browser.quit()
+            sys.exit()
+        if Wait_by_id('top-nav'):
+            print('Logged in')
+            break
+        else:
+            print('Unable to login...\nExiting..\n')
+            browser.quit()
+            sys.exit()
+    time.sleep(.2)
